6af7c574e99a334028b50fcff9905a81a54ad44c
R3laxation <memories@bk.ru>
2022-07-24 09:37:57 +0600

    Updates

---
+++ precache-manifest.11fcce9f1a353c67e553e5e2114f7a0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c0d9a8384d76fb9e0ffb6c01d625429",
+    "url": "/todoList2021/index.html"
+  },
+  {
+    "revision": "d6365bc2503aaffc5c6e",
+    "url": "/todoList2021/static/css/main.155097fa.chunk.css"
+  },
+  {
+    "revision": "eedc512af3c91e6cc62b",
+    "url": "/todoList2021/static/js/2.317bd65b.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todoList2021/static/js/2.317bd65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6365bc2503aaffc5c6e",
+    "url": "/todoList2021/static/js/main.2233e7e2.chunk.js"
+  },
+  {
+    "revision": "0b5c90d52d2b30d0da96",
+    "url": "/todoList2021/static/js/runtime-main.ab548751.js"
+  }
+]);
